d815a4b053ba2289dafbd5bcdfcc25269ee712f8
Christopher.Donovan <christopher.donovan@uvm.edu>
2025-03-21 10:35:15 -0400

    finishing lab9

--- lab9_panel_data_1/lab9_assignment.R
+++ lab9_panel_data_1/lab9_assignment.R
@@ -7,30 +7,57 @@
 if (!require('pacman')) install.packages('pacman')
 pacman::p_load(
   dplyr,
-  plm
+  plm,
+  tidyr
 )
 
 options(scipen = 999)
 
-# Load Cigar dataset
+# Load Gasoline and Cigar datasets
+data(Gasoline, package = 'plm')
+str(Gasoline)
+?plm::Gasoline
+
 data(Cigar, package = 'plm')
 str(Cigar)
-
-# Filter to 76-77
-Cigar <- pdata.frame(Cigar)
-Cigar_76_77 <- Cigar[Cigar$year %in% c('76', '77'), ]
+?plm::Cigar
 
 
 
 # Assignment --------------------------------------------------------------
 
 
-summary(lm(sales ~ price + ndi, data = Cigar))
+## 1. Reshaping data
 
-Cigar <- pdata.frame(Cigar)
+# a. Convert the Cigar df into 'long' format and save it to a new object. The
+# first two columns should be country and year, and everything else should be
+# pivoted longer.
+
+
+# b. Convert your 'long' Cigar df back into a 'wide' df. It should be the same
+# as the original Cigar data.
+
+
+
+## 2. Use a first differenced model to explore the effect cigarette prices on
+# cigarette sales (sales ~ price), and interpret the results. You can use either
+# the manual method with lm() or the plm() method. Some set up is done for you
+# below.
+
+# Convert Cigar to pdata.frame
+Cigar <- plm::pdata.frame(Cigar)
 str(Cigar)
 
-summary(lm(sales ~ state + year + price, data = Cigar))
-summary(plm(sales ~ state + year + price, data = Cigar, model = 'within'))
+# Set up two separate DFs for different years
+Cigar_91 <- Cigar[Cigar$year == '91', ]
+Cigar_92 <- Cigar[Cigar$year == '92', ]
 
+# Set up a single DF with both years
+Cigar_91_92 <- Cigar[Cigar$year %in% c('91', '92'), ]
+
+
+
+## 3. Run a fixed effect model with plm() testing sales ~ price on the whole
+# Cigar dataset. Interpret the output. Feel free to add other covariates if you
+# like.
 
--- lab9_panel_data_1/lab9_demo.R
+++ lab9_panel_data_1/lab9_demo.R
@@ -1,49 +1,64 @@
-# Lab 8 Demo
-# Limited Dependent Variables
+# Lab 9 Demo
+# Panel Data 1
 
 
 # Housekeeping ------------------------------------------------------------
 
 
 pacman::p_load(
-  AER,
   dplyr,
-  plm,
-  tidyr
+  AER,
+  plm,    # linear models for panel data
+  tidyr   # reshaping data frames
 )
 
 options(scipen = 999)
+
+# Reminders
+# 1. Midterm things, syntax, responses
+# 2. Class topic for 4.25
 
 
 
 # Tidy Data ---------------------------------------------------------------
 
 
-# Load some example data
+# 1. Each variable is a column, each column is a variable
+# 2. Each observation is a row, each row is an observation
+# 3. Each value is a cell, each cell is a single value
+# More at https://tidyr.tidyverse.org/articles/tidy-data.html#defining
+
+
+## Load some example data
 data(world_bank_pop, package = 'tidyr')
 str(world_bank_pop)
-head(world_bank_pop)
-# This is wide - multiple values per row
 
-# Pivot longer so each row has one piece of information
+# Check first few rows
+head(world_bank_pop, 10)[, 1:6]
+
+
+## Pivot longer to group the year columns
 long <- tidyr::pivot_longer(
   world_bank_pop,
-  cols = starts_with('2'),
+  cols = '2000':'2017',
+  # cols = starts_with('2'),
   names_to = 'year',
   values_to = 'value'
 )
 str(long)
 head(long, 10)
 
-# Pivot it back
+
+## Pivot wider so each column is a variable
 wide <- tidyr::pivot_wider(
   long,
-  id_cols = c('country', 'indicator'),
-  names_from = 'year',
+  id_cols = c('country', 'year'),
+  names_from = 'indicator',
   values_from = 'value'
 )
 str(wide)
 head(wide)
+# This is tidy data
 
 
 
@@ -58,13 +73,17 @@ data(Fatalities, package = 'AER')
 
 # Check out data frame
 str(Fatalities)
-head(Fatalities, 15)[, 1:10]
+head(Fatalities, 10)[, 1:10]
+# Is it tidy?
 
 # Make it a panel data object
-Fatalities <- pdata.frame(Fatalities)
-pdim(Fatalities)
+Fatalities <- plm::pdata.frame(Fatalities)
 str(Fatalities)
 head(Fatalities, 15)[, 1:10]
+
+# pdim is like dim, but gives panel data info
+dim(Fatalities)
+plm::pdim(Fatalities)
 
 # Create a fatality rate variable
 Fatalities$fatal_rate <- Fatalities$fatal / Fatalities$pop * 10000
@@ -76,17 +95,27 @@ Fatalities$fatal_rate <- Fatalities$fatal / Fatalities$pop * 10000
 
 
 # No entity specific effects, same relationship across all years and states
-# Cannot account for shocks
-
-# Plain old lm
 summary(lm(fatal_rate ~ beertax, data = Fatalities))
 
-summary(plm(
+# We can add year as a factor as well
+summary(lm(fatal_rate ~ beertax + year, data = Fatalities))
+
+# We can add interactions to allow for different slopes
+summary(lm(fatal_rate ~ beertax * year, data = Fatalities))
+
+
+## Now with plm package
+model_pooled <- plm::plm(
   fatal_rate ~ beertax,
   data = Fatalities,
+  index = c('state', 'year'), # If null, assumes first two columns
   model = 'pooling'
-))
+)
+summary(model_pooled)
 
+
+## Chow test
+pooltest(fatal_rate ~ beertax, data = Fatalities, model = 'pooling')
 
 
 
@@ -108,12 +137,14 @@ diff_beertax <- Fatalities1988$beertax - Fatalities1982$beertax
 model_diff <- lm(diff_fatal_rate ~ diff_beertax)
 summary(model_diff)
 # Raising beer tax by $1 drops fatalities by 1.04 per 10,000
-# Within state effect
+# Different than pooled, which ignores state-specific effects
 
 
 ## Now with plm
 # Filter data to the same two years
 Fatalities_82_88 <- Fatalities[Fatalities$year %in% c('1982', '1988'), ]
+
+# Run equivalent model
 model_fd <- plm(
   fatal_rate ~ beertax,
   data = Fatalities_82_88,
@@ -128,9 +159,8 @@ summary(model_fd)
 
 
 # Allows for individual differences (different intercepts)
-# remove alpha_i by de meaning or including dummies
-# Different intercepts for each state
 # Controls for unobserved, time invariant effects
+# remove alpha_i by de meaning rather than differencing
 
 model_fe <- plm(
   fatal_rate ~ beertax,
@@ -139,15 +169,5 @@ model_fe <- plm(
   model = 'within' # for fixed effects
 )
 summary(model_fe)
-
-
-test <- plm(
-  fatal_rate ~ beertax + state,
-  data = Fatalities,
-  index = c('state', 'year'),
-  model = 'within' # for fixed effects
-)
-summary(test)
-
 
 
