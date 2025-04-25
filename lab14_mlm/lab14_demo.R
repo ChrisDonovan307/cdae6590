ca86ff6b3945c5426ddadddfe789446f45edb224
Christopher.Donovan <christopher.donovan@uvm.edu>
2025-04-24 20:55:04 -0400

    updates to lab14 demo

--- lab14_mlm/lab14_demo.R
+++ lab14_mlm/lab14_demo.R
@@ -16,13 +16,31 @@ pacman::p_load(
   stringr,
   glue,
   tidyr,
-  performance
+  performance,
+  rnassqs
 )
 
 options(scipen = 999)
 
 
-# API Example -------------------------------------------------------------
+# Using APIs --------------------------------------------------------------
+
+
+# APIs are how programs communicate
+# GET, POST, PUT, DELETE, etc.
+
+# Guidelines
+#   Read the documentation
+#   Only take what you need
+#   Cache with memoise()
+#   Consider adding timers with Sys.sleep()
+#   Don't hard code your API key
+
+# Great resources for APIs in R:
+#   https://paulcbauer.github.io/apis_for_social_scientists_a_review/
+
+
+
 ## Search by Category ------------------------------------------------------
 
 
@@ -68,9 +86,13 @@ get_meta <- function(domain, limit) {
 out <- get_meta(domain = 'data.vermont.gov', limit = 10)
 str(out)
 
+# Remove objects from environment
+rm(list = ls())
 
 
-# NASS API ----------------------------------------------------------------
+
+# NASS Dataset ------------------------------------------------------------
+## API ---------------------------------------------------------------------
 
 
 # Load API key. This won't work for you - get your own key!
@@ -85,7 +107,6 @@ nassqs_params()
 
 # Check options for parameters
 nassqs_param_values('sector_desc')
-nassqs_param_values('short_desc')
 
 # Set variables to query
 (short_descs <- nassqs_param_values(param = 'short_desc'))
@@ -119,6 +140,9 @@ saveRDS(out, 'datasets/nass_mlm_out.rds')
 ## Clean NASS Data ---------------------------------------------------------
 
 
+# Pull saved data from API out
+out <- readRDS('datasets/nass_mlm_out.rds')
+
 # Check variables
 unique(out$short_desc)
 get_str(out)
@@ -147,7 +171,7 @@ get_str(dat)
 
 
 
-# Wrangle State Data ------------------------------------------------------
+## Wrangle State Data ------------------------------------------------------
 
 
 # Pull BEA data
@@ -179,7 +203,7 @@ get_str(state_dat)
 
 
 
-# Joins -------------------------------------------------------------------
+## Joins -------------------------------------------------------------------
 
 
 str(dat)
@@ -188,37 +212,167 @@ str(state_dat)
 dat <- inner_join(dat, state_dat)
 str(dat)
 
-# Rescale
-dat <- dat %>%
-  mutate(across(c(no_till, tourism, income), ~ scale(.x)))
 
-
-
-# Model Progression -------------------------------------------------------
+## Imputation --------------------------------------------------------------
 
 
 get_str(dat)
 
+# Has to be base data.frame, not tibble
+# Only takes numeric and factor variables
+input <- dat %>%
+  select(-county) %>%
+  mutate(state = factor(state)) %>%
+  as.data.frame()
+str(input)
+
+# Impute
+pacman::p_load(missForest)
+out <- missForest(
+  input,
+  ntree = 100,
+  variablewise = TRUE,
+  verbose = TRUE
+)
+
+out$OOBerror
+str(out$ximp)
+
+# Join back with counties
+clean <- bind_cols(dat['county'], out$ximp)
+str(clean)
+
+# Save to dataset folder
+saveRDS(clean, 'datasets/lab14_nass_data.rds')
+rm(list = ls())
+
+
+
+# Multilevel Models -------------------------------------------------------
+
+
+# Pull clean dataset from GitHub
+github_url <- 'https://raw.githubusercontent.com/ChrisDonovan307/cdae6590/refs/heads/main/datasets/fsci_data.rds'
+con <- gzcon(url(github_url, 'rb'))
+fsci <- readRDS(con)
+close(con)
+
+# Pull from datasets folder
+nass <- readRDS('datasets/lab14_nass_data.rds')
+str(nass)
+
+#' no-till: Total acres in no-till conservation practice
+#' tourism: Total income from Ag tourism and recreation
+#' income: Total income from farm-related activities in $ per operation
+
+
+
+## Context -----------------------------------------------------------------
+
+
+# FE/RE remove unobserved effects, entity specific characteristics
+# FE: model within effect only, control for between
+# RE: include random intercept, but interpret as blend of within and between
+# MLM: explicitly model BOTH the within effects AND between effects
+#   Does the slope vary by group?
+
+# Assumption of OLS: IID Residuals (independent and identical)
+#   OLS is naive to groupings
+#   Ecological fallacy - apply group level results to individual level
+#   Atomistic fallacy - apply individual level results to group level
+
+# Limitations:
+#   Need large sample size
+#   Convergence problems
+#   Scaling
+#   Worth mentioning spatial regression
+
+# If you don't have a multi-level question, don't use a multi-level model
+#   Cluster robust standard errors
+#   Groups as fixed effects
+
+# Extensions
+#   Well suited for repeated measure data
+#   MRPS: Multi-level regression with post-stratification
+
+
+
+## Model Progression -------------------------------------------------------
+### Null Model --------------------------------------------------------------
+
+
+str(df)
+
 ## Null model, intercept only
-null <- lmer(income ~ 1 + (1 | state), data = dat)
+null <- lme4::lmer(income ~ 1 + (1 | state), data = nass)
 
 # Check intra class correlation
-icc(null)
-# 22% of variation is at state level, 78% at county level
+performance::icc(null)
+# 25% of variation is at state level, 75% at county level
+# Adjusted: only random effect variances
+# Unadjusted: includes fixed effect variances (but there are none)
 
 
-## Add predictors to level 1
-lmer1 <- lme4::lmer(income ~ no_till + (1 | state), data = dat)
+
+
+### Add Level 1 Predictor ---------------------------------------------------
+
+
+## Add predictor to level 1
+lmer1 <- lme4::lmer(income ~ no_till + (1 | state), data = nass)
+# Scaling issues
+
+# Rescale
+df <- nass %>%
+  mutate(across(where(is.numeric), scale))
+
+# Try again
+lmer1 <- lme4::lmer(income ~ no_till + (1 | state), data = df)
 summary(lmer1)
-# Notice there are no p-values
+# Notice there are no p-values. Great resource on this and much more with GLMMs:
+# https://bbolker.github.io/mixedmodels-misc/glmmFAQ.html
 
-# Other packages will give us those
-lmer1 <- lmerTest::lmer(income ~ no_till + (1 | state), data = dat)
+# Other packages can give us p-values though:
+lmer1 <- lmerTest::lmer(income ~ no_till + (1 | state), data = df)
+
+# Compare null model to no_till model
+anova(null, lmer1)
 summary(lmer1)
 
+# Check R2
+performance::r2(lmer1)
+# Marginal: Fixed effects only
+# Conditional: Both fixed and random effects
 
-## Add random slope
-lmer2 <- lmerTest::lmer(income ~ no_till + (1 + tourism | state), data = dat)
-summary(lmer2)
 
+## Plot it
+df %>%
+  ggplot(aes(x = no_till, y = income, color = state)) +
+  geom_point(alpha = 0.6) +
+  geom_smooth(aes(group = state), method = "lm", se = FALSE, fullrange = TRUE) +
+  coord_cartesian(xlim = c(0, 100000), ylim = c(0, 200000)) +
+  theme_classic() +
+  theme(legend.position = 'None')
+
+
+
+### Add More Predictors -----------------------------------------------------
+
+
+## Add tourism as a first-level predictor
+lmer2 <- lmerTest::lmer(income ~ no_till + tourism + (1 | state), data = df)
+
+# Compare models
 anova(lmer1, lmer2)
+summary(lmer2)
+# Level 2 intercept: average value of income
+# variance: how much states vary around average income
+
+
+## Add tourism as a random slope
+lmer3 <- lmerTest::lmer(income ~ no_till + tourism + (tourism | state), data = df)
+
+# Compare
+anova(lmer2, lmer3)
+summary(lmer3)
+# Correlation is between slope of tourism and intercept of tourism
